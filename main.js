3931e8b6687def2832ffd4db77bad9119ac2b2b7
Sebastian <sebastian.maya0118@gmail.com>
2022-12-26 00:58:08 -0600

    Init commit
    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+*/ 
+
+if(){
+    some=SVGFEDropShadowElement
+    if(){
+        if(){
+
+        }else{
+            if(){
+                function something(){
+                    Array.array.forEach(elemnt)
+
+                }); 
+                }
+            }
+        }
+    }
+}
+
+//? What is this 
+//! WARNING
+// Hello 
